6d53a6cb2367bcd676bd79c8ae2ed6b6679afacc
Durai Velan <durai.9.velan@stackroute.in>
2022-01-19 18:49:02 +0530

    Boilerplate

---
+++ src/main/java/com/stackroute/oops/Account.java
@@ -0,0 +1,42 @@
+package com.stackroute.oops;
+
+/*
+   This class is used to store account details
+ */
+public class Account {
+
+    public Account(double balance, String status) {
+    }
+
+    public boolean depositAmount(double amount) {
+        return false;
+    }
+
+    public String getAccountDetails() {
+        return null;
+    }
+
+    public static int getCounter() {
+        return -1;
+    }
+
+    public static void setCounter(int counter) {
+
+    }
+
+    public int getAccountNo() {
+        return -1;
+    }
+
+    public double getBalance() {
+        return -1;
+    }
+
+    public String getStatus() {
+        return null;
+    }
+
+    public void setStatus(String status) {
+
+    }
+}
---
+++ src/main/java/com/stackroute/oops/AccountService.java
@@ -0,0 +1,28 @@
+package com.stackroute.oops;
+
+/*
+	This class is used to create account object and execute methods on account objects
+ */
+public class AccountService {
+
+	/*
+	 * This method creates and returns an account object based on the parameters supplied
+	 */
+	public Account createAccount(double balance, String status){
+		return null;
+	}
+
+	/*
+	 * This method deposits the parameter amount in the account passed as parameter
+	 */
+	public boolean depositAmountInAccount(Account account, double amount){
+		return false;
+	}
+
+	/*
+	 * This method gets the account details for the  account passed as parameter
+	 */
+	public String getAccountDetails(Account account){
+		return null;
+	}
+}
